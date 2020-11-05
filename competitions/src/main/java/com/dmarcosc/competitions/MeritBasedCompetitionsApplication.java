5641831d79ca75c1ae30cd83a8cee2623e60dfeb
Usuario <Usuario@DESKTOP-IG1EPJQ>
2020-11-06 00:45:55 +0100

    First commit, base structure, user/roles and jwt security

---
+++ competitions/src/main/java/com/dmarcosc/competitions/MeritBasedCompetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.dmarcosc.competitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeritBasedCompetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeritBasedCompetitionsApplication.class, args);
+	}
+
+}
